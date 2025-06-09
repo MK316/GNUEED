7f8fa603b662efe4727bc78fc7eee6185eee4514
MK <99416359+MK316@users.noreply.github.com>
2025-06-09 15:46:06 +0900

    Update HOME.py

--- HOME.py
+++ HOME.py
@@ -3,3 +3,8 @@ import streamlit as st
 st.title("🎥 Microteaching Video Gallery")
 st.write("This application provides curated microteaching video samples to support your growth as future English educators. You can watch real teaching demonstrations, observe different classroom strategies, and reflect on various teaching styles. Use the dropdown menus to explore lessons by topic or technique, and take notes to guide your own microteaching practice.")
 st.caption("Under Construction; since June 9, 2025")
+
+url1="https://github.com/MK316/GNUEED/raw/main/images/MThome.png"
+url2="https://github.com/MK316/GNUEED/raw/main/images/GNU.png"
+st.image(url1)
+st.image(url2, width=200)
