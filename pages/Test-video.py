ba204cac2767ef5599ddaa3b6a89625b19912206
MK <99416359+MK316@users.noreply.github.com>
2025-06-09 15:30:26 +0900

    Update Test-video.py

--- pages/Test-video.py
+++ pages/Test-video.py
@@ -10,3 +10,14 @@ video_html = f"""
 
 st.markdown("### 📹 Video Preview from Google Drive", unsafe_allow_html=True)
 st.markdown(video_html, unsafe_allow_html=True)
+
+
+file_id2 = "1roW9ovZC7UA8mncElEdz0WFVURZwcOny"
+# https://drive.google.com/file/d/1sJ6zdP-6-lW__m8y4lX-qRQhnVUoTYGc/view?usp=sharing
+# Embed video using an iframe (Google Drive preview)
+video_html = f"""
+<iframe src="https://drive.google.com/file/d/{file_id2}/preview" width="640" height="360" allow="autoplay"></iframe>
+"""
+
+st.markdown("### 📹 Video Preview from Google Drive 2", unsafe_allow_html=True)
+st.markdown(video_html, unsafe_allow_html=True)
