46f91840ef21ce44e1491c1ebb2ada6016fa5248
MK <99416359+MK316@users.noreply.github.com>
2025-06-09 15:46:31 +0900

    Update HOME.py

--- HOME.py
+++ HOME.py
@@ -6,5 +6,5 @@ st.caption("Under Construction; since June 9, 2025")
 
 url1="https://github.com/MK316/GNUEED/raw/main/images/MThome.png"
 url2="https://github.com/MK316/GNUEED/raw/main/images/GNU.png"
-st.image(url1)
+st.image(url1, width=300)
 st.image(url2, width=200)
