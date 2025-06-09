c32b34754efb9e78982878c44d4284ae879f9c75
MK <99416359+MK316@users.noreply.github.com>
2025-06-09 15:25:32 +0900

    Create Test-video.py

---
+++ pages/Test-video.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+# Google Drive video ID
+file_id = "1sJ6zdP-6-lW__m8y4lX-qRQhnVUoTYGc"
+# https://drive.google.com/file/d/1sJ6zdP-6-lW__m8y4lX-qRQhnVUoTYGc/view?usp=sharing
+# Embed video using an iframe (Google Drive preview)
+video_html = f"""
+<iframe src="https://drive.google.com/file/d/{file_id}/preview" width="640" height="360" allow="autoplay"></iframe>
+"""
+
+st.markdown("### 📹 Video Preview from Google Drive", unsafe_allow_html=True)
+st.markdown(video_html, unsafe_allow_html=True)
